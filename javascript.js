0366f3f4a161e17ebbb058584a8c0a39cd413bd9
yoricktf <yorick.tenfeld@gmail.com>
2021-05-22 11:36:50 +0200

    made a nonworking function to try and change the background of the cells

--- javascript.js
+++ javascript.js
@@ -1,6 +1,14 @@
 for(x=0; x<256;x++) {
-    var cell = document.createElement('div');
+    const cell = document.createElement('div');
     cell.className = "cells";
-
+    
     document.getElementById('container').appendChild(cell);
 }
+
+document.addEventListener("mouseenter", function (event){
+const toFill = document.getElementsByClassName("cells");
+toFill.className += ' red';
+});
+
+
+
