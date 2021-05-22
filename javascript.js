ee1e8ab45c15b140ba9daadc275fc2a7d2cf8486
yoricktf <yorick.tenfeld@gmail.com>
2021-05-22 12:07:27 +0200

    some html elements and css formatting

--- javascript.js
+++ javascript.js
@@ -2,13 +2,16 @@ for(x=0; x<256;x++) {
     const cell = document.createElement('div');
     cell.className = "cells";
     
-    document.getElementById('container').appendChild(cell);
+    document.getElementById('board').appendChild(cell);
 }
+// WHAT IS IN GIT FOR KOFFA TO SEE
+// document.addEventListener("mouseenter", function (event){
+// const toFill = document.getElementsByClassName("cells");
+//  += ' red';
+// });
+
 
 document.addEventListener("mouseenter", function (event){
 const toFill = document.getElementsByClassName("cells");
-toFill.className += ' red';
+
 });
-
-
-
